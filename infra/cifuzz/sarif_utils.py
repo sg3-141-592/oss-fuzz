d32b6514b3bbb3b69dd323a463f9aae04b8e0c42
Jonathan Metzman <metzman@chromium.org>
2021-07-30 06:13:02 -0700

    tmp

---
+++ infra/cifuzz/sarif_utils.py
@@ -0,0 +1,62 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Module for outputting SARIF data."""
+BUG_ID = 'bug'
+RULES = [
+  {
+      'id': '1',  # Needs to be a stable, opaque identifier.
+      'name': BUG_ID,
+      'shortDescription': {
+        'text': 'A bug'
+      },
+      'fullDescription': {
+        'text': 'A bug'
+      },
+      'help': {
+        'text': 'A bug'
+      }
+  }
+]
+
+def get_sarif_data(stacktrace, artifact_url):
+  result = {
+      'ruleId': BUG_ID,
+      'partialFingerprints': {  # TODO(metzman): Use the stackframes.
+        'primaryLocationLineHash': ''
+      },
+      'message': {
+        'text': 'A bug'
+      }
+  }
+  # TODO(metzman): Add location.
+  run = {
+      'tool': {
+          'driver': {
+              'name': 'CIFuzz',
+              'rules': RULES,
+              # TODO(metzman): Finish this.
+              'informationUri': 'https://CIFuzz/Docs',
+          },
+      },
+      'results': [result],
+  }
+
+
+  data = {
+      '$schema': ('https://raw.githubusercontent.com/oasis-tcs/sarif-spec/'
+                  'master/Schemata/sarif-schema-2.1.0.json'),
+      'version': '2.1.0', # !!! Delete?
+      'runs': [run]
+  }
+  return data
