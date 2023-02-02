0a6b7efe1133889a704a902c1511c441af1ff4a8
Jonathan Metzman <metzman@chromium.org>
2023-02-02 12:17:40 -0500

    Try again

--- infra/cifuzz/sarif_utils.py
+++ infra/cifuzz/sarif_utils.py
@@ -78,9 +78,11 @@ def get_frame(crash_info):
   frames = crash_info.frames[0]
   if not frames:
     return
+  from remote_pdb import RemotePdb; RemotePdb('127.0.0.1', 4445).set_trace()
+
   print('state', state, crash_info.crash_state)
   for frame in frames:
-    if frame.function_name == state:
+    if frame.function_name.startswith(state): # !!! buggy
       return frame
   return None
 
